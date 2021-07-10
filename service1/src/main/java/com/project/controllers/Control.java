0a948226ddc0d44b64301a6f97a2942173e5c127
alaajamal-git <alaa.jamal@stud.uni-obuda.hu>
2021-07-10 14:39:07 +0200

    docker-compose

--- service1/src/main/java/com/project/controllers/Control.java
+++ service1/src/main/java/com/project/controllers/Control.java
@@ -1,5 +1,7 @@
 package com.project.controllers;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/control")
 public class Control {
-	
+	private final int id=(int)(Math.random()*20);
 	@PostMapping("/add")
-	public ResponseEntity<String> response(@RequestBody String temp) {
-		return new ResponseEntity<String>("Working",HttpStatus.ACCEPTED);
+	public ResponseEntity<String> response(@RequestBody String temp,HttpServletResponse res) {
+		
+		return new ResponseEntity<String>("Working+ "+id,HttpStatus.ACCEPTED);
 	}
 	
 	
